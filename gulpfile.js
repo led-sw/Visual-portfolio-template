ed1cfc374561e9967aeb8f0aff9ca5ba218524cb
led-sw <lednevsoftware@gmail.com>
2021-11-16 16:32:24 +0300

    first

---
+++ gulpfile.js
@@ -0,0 +1,136 @@
+let projectFolder = require('path').basename(__dirname);
+let sourceFolder = '#src';
+
+let path = {
+    build: {
+        html: projectFolder + '/',
+        css: projectFolder + '/css/',
+        js: projectFolder + '/js/',
+        img: projectFolder + '/img/',
+        fonts: projectFolder + '/fonts/',
+    },
+    src: {
+        html: [sourceFolder + '/*.html', '!' + sourceFolder + '/_*.html'],
+        css: sourceFolder + '/scss/style.scss',
+        js: sourceFolder + '/js/script.js',
+        img: sourceFolder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
+        fonts: sourceFolder + '/fonts/*.ttf',
+    },
+    watch: {
+        html: sourceFolder + '/**/*.html',
+        css: sourceFolder + '/scss/**/*.scss',
+        js: sourceFolder + '/js/**/*.js',
+        img: sourceFolder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
+    },
+    celan: './' + projectFolder + '/',
+};
+
+let {src, dest} = require('gulp'),
+    gulp = require('gulp'),
+    browsersync = require('browser-sync').create(),
+    fileinclude = require('gulp-file-include'),
+    del = require('del'),
+    scss = require('gulp-sass')(require('sass')),
+    autoprefixer = require('gulp-autoprefixer'),
+    groupMedia = require('gulp-group-css-media-queries'),
+    cleanCss = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify-es').default,
+    webp = require('gulp-webp'),
+    webpHtml = require('gulp-webp-html'),
+    webpCss = require('gulp-webp-css');
+
+    function browserSync() {
+        browsersync.init({
+            server: {
+                baseDir: './' + projectFolder + '/',
+            },
+            port: 3000,
+            notify: false,
+        })
+    }
+
+    function html() {
+        return src(path.src.html)
+            .pipe(fileinclude())
+            .pipe(webpHtml())
+            .pipe(dest(path.build.html))
+            .pipe(browsersync.stream())
+    }
+
+    function css() {
+        return src(path.src.css)
+            .pipe(
+                scss({
+                    outputStyle: 'expanded'
+                })
+            )
+            .pipe(
+                groupMedia()
+            )
+            .pipe(
+                autoprefixer({
+                    overrideBrowserslist: ['last 5 versions'],
+                    cascade: true,
+                })
+            )
+            .pipe(webpCss())
+            .pipe(dest(path.build.css))
+            .pipe(cleanCss())
+            .pipe(
+                rename({
+                    extname: '.min.css'
+                })
+            )
+            .pipe(dest(path.build.css))
+            .pipe(browsersync.stream())
+    }
+
+    function js() {
+        return src(path.src.js)
+            .pipe(fileinclude())
+            .pipe(dest(path.build.js))
+            .pipe(
+                uglify()
+            )
+            .pipe(
+                rename({
+                    extname: '.min.js'
+                })
+            )
+            .pipe(dest(path.build.js))
+            .pipe(browsersync.stream())
+    }
+
+    function images() {
+        return src(path.src.img)
+            .pipe(
+                webp({
+                    quality: 70
+                })
+            )
+            .pipe(dest(path.build.img))
+            .pipe(browsersync.stream())
+    }
+
+    function watchFiles() {
+        gulp.watch([path.watch.html], html);
+        gulp.watch([path.watch.css], css);
+        gulp.watch([path.watch.js], js);
+        gulp.watch([path.watch.img], images);
+    }
+    
+    function clean(params) {
+        return del(path.celan);
+    }
+
+    let build = gulp.series(clean, gulp.parallel(js, css, html, images));
+    let watch = gulp.parallel(build, watchFiles, browserSync);
+
+    exports.images = images;
+    exports.js = js;
+    exports.css = css;
+    exports.html = html;
+    exports.build = build;
+    exports.watch = watch;
+    exports.default = watch;
